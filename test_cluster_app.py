1806cd26655b263727dcb5cc6007a09f6a451dcb
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-13 16:26:46 -0500

    fix "positive-semidefinite" warning by using diagonal for covariance

--- test_cluster_app.py
+++ test_cluster_app.py
@@ -19,17 +19,22 @@ class ClusterAppTestCase(unittest.TestCase):
     def test_medium_matrix(self):
         def random_matrix(means, rows):
             cols = len(means)
-            covariance = [[1] * cols] * cols
+            covariance = np.diagflat([1] * cols)
             matrix = np.random.multivariate_normal(means, covariance, rows)
             return matrix.round(2).tolist()
+
         np.random.seed(1)
         cols = 5
         rows = 10
-        combined = random_matrix([10]*cols, rows) + random_matrix([20]*cols, rows)
+        combined = random_matrix([10] * cols, rows) + random_matrix([20] * cols, rows)
         combined_json = json.dumps(combined)
         r = self.app.get('/cluster?matrix=%s' % combined)
         permutation = json.loads(r.data)
-        self.assertEqual(permutation, [14, 15, 11, 18, 12, 13, 16, 10, 17, 19, 0, 2, 1, 7, 8, 3, 4, 9, 5, 6])
+        # The important part is that the two chunks are segregated;
+        # precise order will be different with different random seed.
+        self.assertTrue(all([x >= 10 for x in permutation[:10]]))
+        self.assertTrue(all([x < 10 for x in permutation[-10:]]))
+        self.assertEqual(permutation, [13, 14, 15, 11, 12, 16, 17, 19, 10, 18, 0, 2, 1, 6, 3, 5, 7, 8, 4, 9])
 
 
 if __name__ == '__main__':
